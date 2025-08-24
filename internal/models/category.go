00a42255709a5add54a0e6bddacaca80b3e8c0f8
Pragalya Kanakaraj <pragalyakanakaraj@gmail.com>
2025-08-24 13:40:02 +0530

    Modified the Category Model

--- internal/models/category.go
+++ internal/models/category.go
@@ -5,7 +5,7 @@ import "time"
 type Category struct {
 	CategoryID string     `json:"category_id" gorm:"primaryKey;type:uuid;default:gen_random_uuid()"`
 	Name       string     `json:"name"`
-	Type       string     `json:"type"` // "Income" or "Expense"
+	Type       string     `json:"type"` //Like "Income" or "Expense"
 	Icon       *string    `json:"icon,omitempty"`
 	CreatedAt  time.Time  `json:"created_at"`
 	UpdatedAt  time.Time  `json:"updated_at"`
