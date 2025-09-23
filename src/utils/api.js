2aa6b6308b6ada61a534e28688f2f0a6933573c8
Pragalya Kanakaraj <pragalyakanakaraj@gmail.com>
2025-09-23 08:39:53 +0530

    app file is modified

--- src/utils/api.js
+++ src/utils/api.js
@@ -4,7 +4,6 @@ const API = axios.create({
   baseURL: "https://expense-tracker-backend-1-70fb.onrender.com/api", 
 });
 
-// Add Authorization header if token exists
 API.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
   if (token) {
