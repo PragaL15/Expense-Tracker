8be992571fc87aa8446aa6e0322f9f28d4df8b14
Pragalya Kanakaraj <pragalyakanakaraj@gmail.com>
2025-09-24 21:22:56 +0530

    navbar is modified

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
-import { Menu, X } from "lucide-react"; // icons
+import { Menu, X } from "lucide-react"; 
 
 const Sidebar = () => {
   const [open, setOpen] = useState(false);
