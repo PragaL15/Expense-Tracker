731de496e9936e0dae3015b77880a214ab437e82
PragalyaK <123529794+PragaL15@users.noreply.github.com>
2026-05-31 13:42:13 +0530

    Update NavBar.jsx

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -15,7 +15,7 @@ const Sidebar = () => {
     
     { name: "Add Income", path: "/add-income" },
     { name: "Add Expense", path: "/add-expense" },
-    { name: "Dashboard", path: "/BudgetTracking" },
+    { name: "Dashboard", path: "/budgetTracking" },
     { name: "History", path: "/transactionHistory" },
     { name: "Logout", path: "/" }
   ];
